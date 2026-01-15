a217da451ab9f139f48e44fb23912acd4ca305f9
Beniamin Idziak <beniamin.idziak@gmail.com>
2026-01-15 15:42:26 +0100

    Wire new ui in scene delegate

--- Vonage/SceneDelegate.swift
+++ Vonage/SceneDelegate.swift
@@ -10,12 +10,25 @@ import SwiftUI
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
+    private lazy var navigationController = UINavigationController(
+        rootViewController: UIHostingController(
+            rootView: HomeView(action: presentPermissionsViewController)
+        )
+    )
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = UIHostingController(rootView: HomeView{})
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
+    }
+    
+    private func presentPermissionsViewController() {
+        let controller = AVPermissionsController()
+        let model = PermissionsViewModel(controller: controller)
+        let view = PermissionsView(model: model) {}
+
+        navigationController.show(UIHostingController(rootView: view), sender: self)
     }
 }
